1ef4aeb0f24e9c74a73c9503e364d37214fb3b5b
iamedddie <edyolisa@gmail.com>
2023-09-05 21:10:36 +0100

    added main.java file

---
+++ main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer [] arraysOfInts = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        System.out.println(Arrays.toString(arraysOfInts));
+        Arrays.sort(arraysOfInts, Collections.reverseOrder());
+        System.out.println(Arrays.toString(arraysOfInts));
+        String[] arraysOfStrings = {"Edward", "Prince", "Michael", "Williams"};
+        System.out.println(arraysOfStrings[2]);
+        Arrays.sort(arraysOfStrings);
+        System.out.println(Arrays.toString(arraysOfStrings));
+        Arrays.sort(arraysOfStrings, Collections.reverseOrder());
+        System.out.println(Arrays.toString(arraysOfStrings));
+
+    }
+}
